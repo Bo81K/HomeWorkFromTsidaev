cdbf76a097363dc6274ae425f926e9b9cbb7faf7
Bo81K <imnotoleghihihi@gmail.com>
2023-11-03 22:41:14 +0500

    Edit function

--- homework_matplotlib.py
+++ homework_matplotlib.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(0, 2*np.pi, 100)
-y = np.sin(x) * np.cos(x)
+y = np.log(x) + np.cos(x)
 
 plt.plot(x, y)
 plt.xlabel('x')
 plt.ylabel('f(x)')
-plt.title('Plot of sin(x) * cos(x)')
+plt.title('Plot of  log(x) + cos(x)')
 plt.show()
